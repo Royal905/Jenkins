d2f55c644f7a778473c087d76ea11f4cbb20d996
root <root@Ubuntu-kibana.bg0znxu1ivrerhv4xam0nqm11d.bx.internal.cloudapp.net>
2023-04-25 09:41:11 +0000

    deleted

--- headless/app/code/FS/ContactGraphQl/Model/ResourceModel/Contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace FS\ContactGraphQl\Model\ResourceModel;
-class Contact extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
-{
-    protected function _construct()
-    {
-        $this->_init('contact_request', 'id');
-    }
-}
