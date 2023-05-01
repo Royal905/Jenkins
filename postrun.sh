aa63fc283992b00c28208a12d267648a9f5006e2
Royal905 <131137169+Royal905@users.noreply.github.com>
2023-05-01 10:59:40 +0530

    Update postrun.sh

--- postrun.sh
+++ postrun.sh
@@ -4,7 +4,7 @@ echo "Installing havells code"
 # cd to below if code is placed in /var/www/html
 #cd /var/www/html/
 
-cd /var/www/html/headless/
+cd /var/www/html/headless1/
 composer install
 php -d memory_limit=-1 bin/magento s:up
 php -d memory_limit=-1 bin/magento s:d:c
